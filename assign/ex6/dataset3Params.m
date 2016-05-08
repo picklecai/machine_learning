540815ca656318b2cecac0db2f14f79d04696fe5
picklecai <pickle.ahcai@163.com>
2016-05-08 15:06:39 +0800

    update

--- assign/ex6/dataset3Params.m
+++ assign/ex6/dataset3Params.m
@@ -23,10 +23,23 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+C = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+sigma = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+predictions = zeros(yval);
+pre_err = 0;
 
+for i = 1:length(C)
+	for j = 1:length(sigma)
+		model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+		predictions = svmPredict(model, Xval);
+		pre_err(i,j) = mean(double(predictions ~= yval));
+	end
+end
 
+[x,ix] = min(pre_err);
 
-
+C = C(ix(1));
+sigma = sigma(ix(2));
 
 
 % =========================================================================
